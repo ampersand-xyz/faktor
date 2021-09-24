25e340420138047d610e7b282f2b1e9d2e3812db
nickgarfield <ntgarfield@gmail.com>
2021-09-23 23:09:55 -0500

    Restructure project to follow along with Nader Dabit Dev.to post

---
+++ README.md
@@ -0,0 +1,2 @@
+This project was created by following along with this Dev.to post
+https://dev.to/dabit3/the-complete-guide-to-full-stack-solana-development-with-react-anchor-rust-and-phantom-3291
--- programs/faktor/src/lib.rs
+++ programs/faktor/src/lib.rs
@@ -1,14 +1,42 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("HzA2tLg882jYMtTqFTJ8wMUFPW2pTJGbSswg1yK8rASg");
 
 #[program]
-pub mod anchorhello {
+pub mod faktor {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        return Ok(());
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        return Ok(());
     }
 }
 
+// Transaction instructions
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
