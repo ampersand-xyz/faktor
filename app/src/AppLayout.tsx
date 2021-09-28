95333653c2f271583ae65683df4981ab4ceef4cf
Lesley Chang <lesley.hanna98@gmail.com>
2021-09-28 19:35:20 -0400

    Move AppLayout to separate file

---
+++ app/src/AppLayout.tsx
@@ -0,0 +1,17 @@
+import {ReactNode} from 'react';
+
+export const AppLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <body>
+      <main className="h-screen w-screen">
+        <header className="w-screen h-24 py-5 px-8 flex items-center gap-4">
+          <a href="/#">HOME</a>
+          <a href="/#/new">NEW</a>
+          <a href="/#/sent">SENT</a>
+          <a href="/#/received">RECEIVED</a>
+        </header>
+        {children}
+      </main>
+    </body>
+  );
+};
