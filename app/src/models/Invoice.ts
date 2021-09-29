3e77a2f682958352a10603386b032e0a87c9a91b
Lesley Chang <lesley.hanna98@gmail.com>
2021-09-29 10:43:33 -0400

    make invoice model stateless

--- app/src/models/Invoice.ts
+++ app/src/models/Invoice.ts
@@ -1,11 +1,7 @@
-export type RequestStatus = 'pending' | 'approved' | 'rejected';
-
 export interface InvoiceData {
   sender: StringPublicKey;
   recipient: StringPublicKey;
   lamports: number;
   note: string;
   createdAt: number;
-  acceptedAt: number;
-  status: RequestStatus;
 }
