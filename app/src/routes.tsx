6391046e9f5487d93de4e91f6f07838c21e02a87
Lesley Chang <lesley.hanna98@gmail.com>
2021-09-28 20:17:45 -0400

    use BrowserRouter instead of HashRouter

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,15 +1,23 @@
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import { HomeView, NewView, SentView, ReceivedView } from './views';
 
 export const Routes = () => {
   return (
-    <HashRouter basename={'/'}>
+    <Router>
       <Switch>
-        <Route exact path="/" component={() => <HomeView />} />
-        <Route exact path="/new" component={() => <NewView />} />
-        <Route exact path="/sent" component={() => <SentView />} />
-        <Route exact path="/received" component={() => <ReceivedView />} />
+        <Route path="/">
+          <HomeView />
+        </Route>
+        <Route path="/new">
+          <NewView />
+        </Route>
+        <Route path="/received">
+          <ReceivedView />
+        </Route>
+        <Route path="/sent">
+          <SentView />
+        </Route>
       </Switch>
-    </HashRouter>
+    </Router>
   );
 };
