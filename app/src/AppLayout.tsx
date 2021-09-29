2488f101aef18ab8c537b5fa117db240cd0ccb33
Lesley Chang <lesley.hanna98@gmail.com>
2021-09-28 20:23:18 -0400

    Hide navbar if wallet is not connected

--- app/src/AppLayout.tsx
+++ app/src/AppLayout.tsx
@@ -1,14 +1,25 @@
+import { useWallet } from '@solana/wallet-adapter-react';
 import { ReactNode } from 'react';
+
+const Header = () => {
+  const wallet = useWallet();
+
+  if (!wallet.connected) return null;
+
+  return (
+    <header className="w-screen h-24 py-5 px-8 flex items-center gap-4">
+      <a href="/">HOME</a>
+      <a href="/new">NEW</a>
+      <a href="/sent">SENT</a>
+      <a href="/received">RECEIVED</a>
+    </header>
+  );
+};
 
 export const AppLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="h-screen w-screen">
-      <header className="w-screen h-24 py-5 px-8 flex items-center gap-4">
-        <a href="/">HOME</a>
-        <a href="/new">NEW</a>
-        <a href="/sent">SENT</a>
-        <a href="/received">RECEIVED</a>
-      </header>
+      <Header />
       {children}
     </main>
   );
