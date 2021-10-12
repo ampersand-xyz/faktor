18912457dc608b174969504416dae6c82e1ed272
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-12 15:25:13 -0400

    fix small bug from typo

--- app/src/ui/pages/Invoices.tsx
+++ app/src/ui/pages/Invoices.tsx
@@ -140,7 +140,7 @@ export const Invoices = () => {
                           : 'text-gray-500 hover:text-gray-700',
                         'px-3 py-2 font-medium text-sm rounded-md cursor-pointer'
                       )}
-                      aria-current={tab.current ? 'page' : undefined}
+                      aria-current={tab.name ? 'page' : undefined}
                     >
                       {tab.name}
                     </a>
