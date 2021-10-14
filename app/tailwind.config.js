e843098eb6ac7ab329242268e0f38bc34183f0b1
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-14 11:17:35 -0400

    add back global css and tailwind config edits

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,21 +1,51 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultColors = require('tailwindcss/colors');
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  important: true,
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      color: {
-        orange: {
-          500: '#FD833A'
-        },
-        gray: {
-          100: '#EDEDED',
-          900: '#1F1F1F'
-        }
+    colors: {
+      ...defaultColors,
+      orange: {
+        ...defaultColors.orange,
+        500: '#FD833A'
+      },
+      gray: {
+        ...defaultColors.gray,
+        100: '#EDEDED',
+        800: '#292929',
+        900: '#1F1F1F'
+      },
+      indigo: {
+        ...defaultColors.indigo,
+        600: '#3D52D5'
       }
     },
+    extend: {
+      minWidth: {
+        card: '32rem'
+      },
+      width: {
+        card: '32rem'
+      },
+      height: {
+        card: '28rem'
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ['hover', 'disabled'],
+      backgroundColor: ['disabled', 'hover'],
+      backgroundOpacity: ['disabled', 'hover'],
+      textOpacity: ['disabled'],
+      cursor: ['disabled']
+    }
   },
-  plugins: [],
+  plugins: []
 };
