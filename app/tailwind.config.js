e3af97b15de93816bb27aae10b9310633e3785a0
Eliascm17 <Elias.cmoreno17@gmail.com>
2021-10-14 18:23:14 -0500

    added tailwind forms

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const defaultColors = require('tailwindcss/colors');
+const defaultColors = require("tailwindcss/colors");
 module.exports = {
   important: true,
   purge: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './src/**/**/*.{js,jsx,ts,tsx}',
-    './public/index.html'
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -13,39 +13,39 @@ module.exports = {
       ...defaultColors,
       orange: {
         ...defaultColors.orange,
-        500: '#FD833A'
+        500: "#FD833A",
       },
       gray: {
         ...defaultColors.gray,
-        100: '#EDEDED',
-        800: '#292929',
-        900: '#1F1F1F'
+        100: "#EDEDED",
+        800: "#292929",
+        900: "#1F1F1F",
       },
       indigo: {
         ...defaultColors.indigo,
-        600: '#3D52D5'
-      }
+        600: "#3D52D5",
+      },
     },
     extend: {
       minWidth: {
-        card: '32rem'
+        card: "32rem",
       },
       width: {
-        card: '32rem'
+        card: "32rem",
       },
       height: {
-        card: '28rem'
-      }
-    }
+        card: "28rem",
+      },
+    },
   },
   variants: {
     extend: {
-      textColor: ['hover', 'disabled'],
-      backgroundColor: ['disabled', 'hover'],
-      backgroundOpacity: ['disabled', 'hover'],
-      textOpacity: ['disabled'],
-      cursor: ['disabled']
-    }
+      textColor: ["hover", "disabled"],
+      backgroundColor: ["disabled", "hover"],
+      backgroundOpacity: ["disabled", "hover"],
+      textOpacity: ["disabled"],
+      cursor: ["disabled"],
+    },
   },
-  plugins: []
+  plugins: [require("@tailwindcss/forms")],
 };
