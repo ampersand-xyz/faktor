8bc6fee426c1a182947b3013ded3afc8ffea5937
Nick Garfield <nick@and.xyz>
2021-10-15 17:51:28 -0500

    Remove pagination bar from InvoiceTable

--- app/src/components/InvoiceTable.tsx
+++ app/src/components/InvoiceTable.tsx
@@ -156,26 +156,6 @@ export const InvoiceTable = ({
               })}
             </tbody>
           </table>
-          <nav
-            className="flex items-center justify-between px-4 py-3 bg-white border-t border-gray-200 sm:px-6"
-            aria-label="Pagination"
-          >
-            <div className="hidden sm:block">
-              <p className="text-sm text-gray-700">
-                Showing <span className="font-medium">1</span> to{" "}
-                <span className="font-medium">10</span> of{" "}
-                <span className="font-medium">20</span> results
-              </p>
-            </div>
-            <div className="flex justify-between flex-1 sm:justify-end">
-              <a className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md cursor-pointer hover:bg-gray-50">
-                Previous
-              </a>
-              <a className="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md cursor-pointer hover:bg-gray-50">
-                Next
-              </a>
-            </div>
-          </nav>
         </>
       ) : (
         <div className="p-8 bg-white">
