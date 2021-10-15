55a0344f7ee0702cb9bf2b51d646613132973769
Eliascm17 <Elias.cmoreno17@gmail.com>
2021-10-15 16:49:46 -0500

    pay invoice rpc call

---
+++ app/src/api/payInvoice.ts
@@ -0,0 +1,46 @@
+import { BN, Program } from "@project-serum/anchor";
+import { SystemProgram } from "@solana/web3.js";
+import { Invoice } from "src/types";
+import { assertExists } from "src/utils";
+
+export type PayInvoiceRequest = {
+  program?: Program;
+  invoice?: any;
+  amount?: number;
+  debtor?: any;
+};
+
+export const payInvoice = async (req: PayInvoiceRequest): Promise<Invoice> => {
+  // Validate request
+  assertExists(req.program);
+  assertExists(req.invoice);
+  assertExists(req.amount);
+
+  try {
+    await req.program.rpc.pay(new BN(req.amount), {
+      accounts: {
+        invoice: req.invoice.publicKey,
+        creditor: req.invoice.account.creditor,
+        debtor: req.invoice.account.debtor,
+        systemProgram: SystemProgram.programId,
+      },
+    });
+
+    // Return on-chain invoice
+    const invoice = await req.program.account.invoice.fetch(
+      req.invoice.publicKey
+    );
+    return {
+      address: invoice.publicKey,
+      data: {
+        creditor: invoice.creditor,
+        debtor: invoice.debtor,
+        balance: invoice.balance,
+        memo: invoice.memo,
+        issuedAt: invoice.issuedAt,
+      },
+    };
+  } catch (error: any) {
+    throw new Error(`Failed to pay invoice: ${error.message}`);
+  }
+};
