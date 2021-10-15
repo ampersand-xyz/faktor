9543406ba71b1375e794fb61b3303f3b2e577472
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-15 16:38:26 -0400

    make secondary action button have no bg

--- app/src/components/InputField.tsx
+++ app/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import { FormEventHandler } from "react";
+import {FormEventHandler} from "react";
 
 export interface InputFieldProps
   extends React.HTMLAttributes<HTMLInputElement> {
@@ -14,26 +14,26 @@ export interface InputFieldProps
   step?: string;
 }
 
-export const InputField: React.FC<InputFieldProps> = ({
+export const InputField: React.FC<InputFieldProps>=({
   value,
   onChange,
-  type = "text",
+  type="text",
   label,
   placeholder,
-  labelClassName = "text-gray-500 font-medium mb-2",
-  inputClassName = "h-12 text-lg w-full text-black placeholder-gray-400 border border-gray-200 rounded-md px-3 py-2",
-  autoComplete = "off",
+  labelClassName="",
+  inputClassName="",
+  autoComplete="off",
 }) => {
-  const _onChange = (e) => {
+  const _onChange=(e) => {
     onChange(e.target.value);
   };
 
   return (
     <div className={`flex flex-col`}>
-      {label && <label className={labelClassName}>{label}</label>}
+      {label&&<label className={`text-gray-500 font-medium mb-2 ${labelClassName}`}>{label}</label>}
       <input
-        {...{ value, placeholder, autoComplete, type }}
-        className={inputClassName}
+        {...{value,placeholder,autoComplete,type}}
+        className={`h-12 text-lg w-full text-black placeholder-gray-400 border border-gray-200 rounded-md px-3 py-2 ${inputClassName}`}
         onChange={_onChange}
         required
       />
